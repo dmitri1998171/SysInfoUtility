a9200d8e36a3d059aa497c955072653a58f1c0b5
Dmitry <dmitri199817@gmail.com>
2021-01-18 01:19:27 +0700

    add output of individual parameters by arguments

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define arr_size 50
+#define ARR_SIZE 50
 
 struct mem {
 	int memTotal;	// Полный объем
@@ -18,57 +18,24 @@ struct info {
 }info;
 
 struct sys_info {
-    char version[arr_size];      // Версия Линукс
-    char kernel[arr_size];       // Ядро
-	char net_int[arr_size][arr_size];	  // Сетевые интерфейсы
+    char version[ARR_SIZE];      // Версия ядра линукс
+	char net_int[ARR_SIZE][ARR_SIZE]; // Сетевые интерфейсы
+	char cpu[ARR_SIZE];			 		 // Процессор
+	int cpu_cores;		 	 // Кол-во ядер
 	unsigned int count;	  // счетчик кол-ва сетев. инетерфейсов
-	char cpu[arr_size*14];
-
 }sys_info;
 
 FILE *fp;
 
-void openFile(char name[], char attr){
+void openFile(char name[], char attr) {
 	if ((fp = fopen(name, &attr)) == NULL){
 		printf("Не удалось открыть файл\n");
 		exit(-1);}
 }
 
-void version_info() {
-    char ver[] = "/proc/version";
-    openFile(ver, 'r');
-
-    fgets(sys_info.version, arr_size+5, fp);
-    fclose(fp);
-}
-
-void kernel_info() {
-    fgets(sys_info.version, arr_size, fp);
-    fclose(fp);
-}
-
-void network_interaces() {
-	system("rm ./tmp.txt && ls /sys/class/net >> ./tmp.txt");
-
-	char name0[] = "tmp.txt";
-	openFile(name0, 'r');
-
-        // очистка массива для перезаписи
-    memset(sys_info.net_int[arr_size], 0, sizeof(sys_info.net_int[arr_size]));    
-    
-	while (!feof(fp)){
-		fscanf(fp, "%s", sys_info.net_int[sys_info.count]);
-		sys_info.count++;
-	}
-    sys_info.count -= 1;    // исключает ошибку feof - дублирует последнюю строку
-	fclose(fp);
-}
-
-// ##########################################
-
-char* parsing(char *str){
-	char *p = strtok(str," ");
-	p = strtok(NULL, " ");
+char* parsing(char *str, char *symbol) {
+	char *p = strtok(str, symbol);
+	p = strtok(NULL, symbol);
 	return p;
 }
 
@@ -77,61 +44,95 @@ void get_number_from_str(char* str, int* value) {
 	*value = *value / 1024; // перевод Kb->Mb
 }
 
-void mem_info(){
-    char name1[] = "/proc/meminfo";
-	char str[arr_size];
+// ##### HARD INFO ########################
 
-    FILE *f = fopen(name1, "r");
-    while (fgets(str, arr_size, f)) {
-        if(strstr(str, "MemTotal"))
-			get_number_from_str(parsing(str), &info.mem.memTotal);
-        if(strstr(str, "MemAvailable"))
-			get_number_from_str(parsing(str), &info.mem.memAvail);
-        if(strstr(str, "SwapTotal"))
-			get_number_from_str(parsing(str), &info.mem.swapTotal);
-        if(strstr(str, "SwapFree")) {
-			int tmp = 0;
-			get_number_from_str(parsing(str), &tmp);
-			info.mem.swapAvail = info.mem.swapTotal - tmp;
-		}
-    }
-    fclose(f);
+void version_info() {
+    char ver[] = "/proc/version";
+    openFile(ver, 'r');
+    fgets(sys_info.version, ARR_SIZE, fp);
+    fclose(fp);
 }
 
-void cpu_info() {
-	char name2[] = "/proc/loadavg";
-	openFile(name2, 'r');
+void network_interaces() {
+	system("ls /sys/class/net >> ./tmp.txt");
 
-	fgets(info.cpuavg, 15, fp);
+	char name0[] = "tmp.txt";
+	openFile(name0, 'r');
+
+        // очистка массива для перезаписи
+    memset(sys_info.net_int[ARR_SIZE], 0, ARR_SIZE);    
+    
+	while (!feof(fp)){
+		fscanf(fp, "%s", sys_info.net_int[sys_info.count]);
+		sys_info.count++;
+	}
+	// исключает ошибку feof - дублирует последнюю строку
+    sys_info.count -= 1;
 	fclose(fp);
+	system("rm ./tmp.txt");
+}
 
+void cpu_hard_info() {
 	char name3[] = "/proc/cpuinfo";
-	char str[arr_size*4];
+	char str[ARR_SIZE];
+
 	openFile(name3, 'r');
-
-	while(!feof(fp)) {
-		fgets(str, arr_size*4, fp);
-
-		if(strstr(str, "model name") != NULL ){
-			char *p = strtok(str, ":");
-			p = strtok(NULL, ":");
-			strcpy(sys_info.cpu, p);
+	while(fgets(str, ARR_SIZE, fp)) {
+		if(strstr(str, "model name")) {
+			char *d = parsing(str, ":");
+			strcpy(sys_info.cpu, d);
+		}
+		if(strstr(str, "cpu cores")) {
+			char *p = parsing(str, ":");
+			sys_info.cpu_cores = atoi(p);
 			break;
 		}
 	}
 	fclose(fp);
 }
 
-// ##########################################
-
-void get_info() {
+void get_hard_info() {
     version_info();
-    kernel_info();
     network_interaces();
-    
-	mem_info();
-    cpu_info();
+	cpu_hard_info();
 }
+
+// ##### SYSTEM INFO #######################
+
+void cpu_sys_info() {
+	char name2[] = "/proc/loadavg";
+	openFile(name2, 'r');
+	fgets(info.cpuavg, 15, fp);
+	fclose(fp);
+}
+
+void mem_info() {
+    char name1[] = "/proc/meminfo";
+	char str[ARR_SIZE];
+
+    openFile(name1, 'r');
+    while (fgets(str, ARR_SIZE, fp)) {
+        if(strstr(str, "MemTotal"))
+			get_number_from_str(parsing(str, " "), &info.mem.memTotal);
+        if(strstr(str, "MemAvailable"))
+			get_number_from_str(parsing(str, " "), &info.mem.memAvail);
+        if(strstr(str, "SwapTotal"))
+			get_number_from_str(parsing(str, " "), &info.mem.swapTotal);
+        if(strstr(str, "SwapFree")) {
+			int tmp = 0;
+			get_number_from_str(parsing(str, " "), &tmp);
+			info.mem.swapAvail = info.mem.swapTotal - tmp;
+		}
+    }
+    fclose(fp);
+}
+
+void get_sys_info() {
+    cpu_sys_info();
+	mem_info();
+}
+
+// ##### OUTPUT #############################
 
 void current_values_output() {
 	printf("CPU avg: %s\n", info.cpuavg);
@@ -140,52 +141,65 @@ void current_values_output() {
 	printf("Swap: %i / %i Mb\n", info.mem.swapAvail, info.mem.swapTotal);
 }
 
-void out(){
+void out() {
+    printf("\n\tSystem information\n\n");
     printf("Version: %s\n", sys_info.version);
-
     printf("Network interfaces: ");
-	for(int i=0; i < sys_info.count; i++){	
+	
+	for(int i=0; i < sys_info.count; i++)	
 		printf("%s ", sys_info.net_int[i]);
-	}
-
-	printf("\nCPU: %s", sys_info.cpu);
+	
+	printf("\nCPU: %s\n", sys_info.cpu);
+	printf("CPU CORES: %i\n", sys_info.cpu_cores);
 	printf("GPU: \n");
 	printf("RAM: %i Mb\n", info.mem.memTotal);
 	printf("Swap: %i Mb\n", info.mem.swapTotal);
-
-    printf("\n-------------------------------\n");
-
-	current_values_output();
-
 }
 
 void write_to_log() {
 	char filename[] = {"sysInfo.log"};
-
 	openFile(filename, 'w');
+
+    fprintf(fp, "\n\tSystem information\n\n");
 	fprintf(fp, "Version: %s\n", sys_info.version);
-
-    fprintf(fp, "Net Int.:        ");
-	for(int i=0; i < sys_info.count; i++){	
+    fprintf(fp, "Network interfaces: ");
+	
+	for(int i=0; i < sys_info.count; i++)	
 		fprintf(fp, "%s ", sys_info.net_int[i]);
-	}
 
-    fprintf(fp, "\n-------------------------------\n");
+	fprintf(fp, "\nCPU:\t%s", sys_info.cpu);
+	fprintf(fp, "GPU: \n");
+	fprintf(fp, "RAM: %i Mb\n", info.mem.memTotal);
+	fprintf(fp, "Swap: %i Mb\n", info.mem.swapTotal);
+    fprintf(fp, "\n-------------------------------\n\n");
 
+	fprintf(fp, "CPU avg: %s\n", info.cpuavg);
+	fprintf(fp, "GPU:\n");
 	fprintf(fp, "RAM: %i / %i Mb\n", info.mem.memAvail, info.mem.memTotal);
-	fprintf(fp, "CPU:\t%s", info.cpuavg);
-
-    fprintf(fp, "\n-------------------------------\n");
+	fprintf(fp, "Swap: %i / %i Mb\n", info.mem.swapAvail, info.mem.swapTotal);
 
 	fclose(fp);
 }
 
-int main(void){
-    printf("\n\tSystem information\n\n");
+int main(int argc, char *argv[]) {
+	if(argc == 1){
+		get_hard_info();			// сбор хар-к ПК
+		get_sys_info();				// сбор текущих значений
+		out();						// Вывод хар-к ПК
+		printf("\n-------------------------------\n\n");
+		current_values_output();	// Вывод текущих значений
+	}
 
-    get_info();			// сбор инфы
-    out();				// вывод в консоль
-	write_to_log();		// запись в лог
+	int res = 0;
+	while ((res = getopt(argc,argv,"wsl")) != -1) {
+		switch(res) {
+			case 'w': { get_hard_info(); out(); break; }	// Вывод хар-к ПК
+			case 's': { get_sys_info(); current_values_output(); break; }	// Вывод текущ. знач.
+			case 'l': { write_to_log(); break; }	// запись в лог
+			case '?':
+			case 'h': { printf("\n Usage: %s [OPTION]\n\n -w, --hardware  print hardware information\n -s, --system    print system information\n -l, --log       output in \'sysInfo.log\' file\n -h, --help      print this help\n\n By default, without options, the utility displays hardware and system information\n\n", argv[0]); break; }
+        }
+	}
 
     return 0;
 }
